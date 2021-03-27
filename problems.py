1375b91444ace845fb2fb7ce8fe16fa69e4e0eff
jwald3 <59290280+jwald3@users.noreply.github.com>
2021-03-26 23:52:45 -0400

    Update problems.py
    Added day 2 function

--- problems.py
+++ problems.py
@@ -1,3 +1,7 @@
+# Accept an input number
+# Within the range (1, number), determine the numbers that are divisible by either 3 or 5
+# Find the sum of those numbers
+
 def multiples_of_3_and_5(number):
     res = 0
     for num in range(1, number):
@@ -6,4 +10,24 @@ def multiples_of_3_and_5(number):
     return res
 
 
-print(multiples_of_3_and_5(10))
+print(multiples_of_3_and_5(49)) # 543 
+
+# Accept an input number
+# within the range (1, number), determine the numbers within the fibonacci sequence that are even
+# Find the sum of the even numbers
+
+def fiboEvenSum(n):
+    results = 0
+    fib = 1
+    prevFib = 1
+    
+    while fib <= n:
+        if fib % 2 == 0:
+            results += fib
+            
+        fib += prevFib
+        prevFib = fib - prevFib
+        
+    return results
+
+fiboEvenSum(1000) # 798
